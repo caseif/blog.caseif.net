9d8b4916d554dd5f3942db5ac776d0d55b16faf8
Max Roncace <me@caseif.net>
2017-05-01 13:38:25 -0400

    Rewrite about page

--- about.php
+++ about.php
@@ -3,9 +3,17 @@ require('./templates/header.php');
 ?>
 <div id="feed">
 	<p class="heading" style="margin-top: 0; margin-bottom: -16px;">About Me</p>
-	<p>I'm a programmer who sometimes writes/vents on this website. I mainly develop in Java, although I (obviously) do a bit of web development on the side. On that front, I'm familiar with Javascript, PHP, JSP, and Python. (Also, my TI-BASIC skillz are totally off the charts.) The reason I originally got into Java development was so that I could write <a href="https://dev.bukkit.org/profiles/caseif">Bukkit plugins</a> (more in-depth baclstory <a href="http://www.caseif.net/post.php?id=21">here</a>), some of which I still develop/maintain to this day.</p>
+	<p>I'm a student programmer currently attending University of Maryland who sometimes writes/vents on this website. I
+	mainly develop in Java, although I also dabble in other languages (C, C++, Golang) and (obviously) do a bit of web
+	development on the side. On that front, I'm familiar with Javascript, PHP, JSP, and Python. But perhaps what I'm
+	most proud of is my proficiency in none other than TI-BASIC.</p>
 	
-	<p>I also play video games on the occasion. Usually <i>Rocket League</i> or <i>Skyrim</i> (both of which I've put an unhealthy amount of time into), or sometimes just a random indie game.</p>
+	<p>A big part of the reason I got into programming was in order to make my own
+	<a href="https://dev.bukkit.org/profiles/caseif">Minecraft mods</a> (which I'm now doing as freelance work).
+
+	<p>Leisure activities include eating and aimmlessly wandering campus with friends. I also play video games on the
+	occasion. Usually <i>CoD Zombies</i> or <i>Skyrim</i> (both of which I've put an unhealthy amount of time into), or
+	sometimes just a random indie game.</p>
 </div>
 <?php
 require('./templates/footer.php');
