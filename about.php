9fb3f48d05421f67a9dc9cacaa3b4babcae78505
Max Roncace <me@caseif.net>
2017-05-01 13:45:13 -0400

    blyat

--- about.php
+++ about.php
@@ -2,18 +2,19 @@
 require('./templates/header.php');
 ?>
 <div id="feed">
-	<p class="heading" style="margin-top: 0; margin-bottom: -16px;">About Me</p>
+	<article>
+	<header class="heading">About Me</header>
 	<p>I'm a student programmer currently attending University of Maryland who sometimes writes/vents on this website. I
 	mainly develop in Java, although I also dabble in other languages (C, C++, Golang) and (obviously) do a bit of web
 	development on the side. On that front, I'm familiar with Javascript, PHP, JSP, and Python. But perhaps what I'm
-	most proud of is my proficiency in none other than TI-BASIC.</p>
-	
-	<p>A big part of the reason I got into programming was in order to make my own
-	<a href="https://dev.bukkit.org/profiles/caseif">Minecraft mods</a> (which I'm now doing as freelance work).
+	most proud of is my proficiency in none other than TI-BASIC. A big part of the reason I got into programming was in
+	order to make my own <a href="https://dev.bukkit.org/profiles/caseif">Minecraft mods</a> (which I'm now doing as
+	freelance work).
 
 	<p>Leisure activities include eating and aimmlessly wandering campus with friends. I also play video games on the
 	occasion. Usually <i>CoD Zombies</i> or <i>Skyrim</i> (both of which I've put an unhealthy amount of time into), or
 	sometimes just a random indie game.</p>
+	</article>
 </div>
 <?php
 require('./templates/footer.php');
