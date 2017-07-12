c41fdcf3253e78f0c489164355ea225d0e449fb0
caseif <me@caseif.net>
2017-07-12 10:58:03 -0400

    Analytics!

--- templates/header.php
+++ templates/header.php
@@ -82,6 +82,21 @@ function randStr($length = 32) {
 	<script src="/lib/prism/core.js"></script>
 	<script src='https://www.google.com/recaptcha/api.js'></script>
 
+	<?php
+	if (substr($_SERVER['HTTP_HOST'], 0, 4) !== "test") {
+	?>
+		<script>
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+			ga('create', 'UA-101944034-2', 'auto');
+			ga('send', 'pageview');
+		</script>
+	<?php
+	}
+	?>
+
 	<script>
 	var snow = false;
 
